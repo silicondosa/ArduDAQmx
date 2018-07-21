c9a0a556367b8becd767d90ed46277acaa6227d9
Suraj Chakravarthi Raja <solaremperor@users.noreply.github.com>
2018-07-20 18:18:23 -0700

    created a deviceManager struct to manage device lists

---
+++ deviceManager.h
@@ -0,0 +1,25 @@
+//
+// Created by surajcha on 7/20/18.
+//
+
+#ifndef DAQWRAPPER_DEVICEMANAGER_H
+#define DAQWRAPPER_DEVICEMANAGER_H
+#define <NIDAQmx.h>
+
+typedef struct {
+
+
+}deviceProperties;
+
+typedef struct {
+
+
+}channelProperties;
+
+
+typedef struct {
+
+
+}deviceManager;
+
+#endif //DAQWRAPPER_DEVICEMANAGER_H
