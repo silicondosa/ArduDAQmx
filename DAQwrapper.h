4347d1fb4e05e85cf9e43b29260178df084fd522
Suraj Chakravarthi Raja <solaremperor@users.noreply.github.com>
2018-08-09 19:19:09 -0700

    created the structures necessary to operate NI devices, tasks and pins.
    Signed-off-by: Suraj Chakravarthi <cluster.root@gmail.com>

--- DAQwrapper.h
+++ DAQwrapper.h
@@ -5,4 +5,44 @@
 #ifndef CLINKEDLIST_DAQWRAPPER_H
 #define CLINKEDLIST_DAQWRAPPER_H
 
+#include "cLinkedList.h"
+
+typedef enum    taskStatesTAG   {CONFIG, READY, RUNNING, STOPPED} taskStates;
+typedef enum    pinTypesTAG     {AI, AO, DI, DO, CTR} pinTypes;
+
+typedef struct  DAQdeviceTAG {
+    // Device properties
+    unsigned long   pdtNum;
+    unsigned int    chassisNum, slotNum;
+    char            devAddr[256];
+    unsigned int    numAI, numAO, numDI, numDO, numCTR;
+
+    // Task and pin list
+    cLinkedList     taskList, usedPinList;
+} DAQdevice;
+
+typedef struct  DAQtaskTAG {
+    // Task properties
+    taskStates      taskState;
+    DAQdevice       *homeDevice;
+
+    // Pin list
+    cLinkedList     pinList;
+} DAQtask;
+
+
+typedef struct  DAQpinTAG {
+    // Pin properties
+    pinTypes        pinType;
+    char            signalName[32];
+    char            pinAddr[256];
+    DAQdevice       *homeTask;
+    DAQdevice       *homeDevice;
+
+    // Pin linear signal scaling
+    double          signalSlope;
+    double          signalIntercept;
+} DAQpin;
+
+
 #endif //CLINKEDLIST_DAQWRAPPER_H
